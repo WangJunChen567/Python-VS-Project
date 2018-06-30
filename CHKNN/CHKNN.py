5a771e7d44f4580116ee41b89d48c4a938fa8be9
Junchen Wang <31566015+WangJunChen567@users.noreply.github.com>
2018-06-30 19:45:09 +0800

    fix problem

--- CHKNN/CHKNN.py
+++ CHKNN/CHKNN.py
@@ -2,7 +2,6 @@ from pandas import read_csv
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
-#import seaborn
 
 def load_data():
     yield read_csv(r"./data/DS1_4.csv", header=None)
@@ -20,50 +19,32 @@ def calculate_nearest_neighbors(D):
         NN[i] = np.argsort(DM[i])
     return NN
 
-def within_k_nearest(center, tocheck, NN, k):
-    flag = False
-    for i in range(1,k+1):
-        if NN[center,i] == tocheck:
-            flag = True
-            break
-    if flag:
-        return True
-    else:
-        return False
-
 def calculate_affinity_matrix(D, P_1, P_2):
     n = len(D)
     A_h = np.zeros((n, n), dtype = int)
     NN = calculate_nearest_neighbors(D)
     for i in range(n):
-        for j in range(n):
-            if i == j:
-                A_h[i,j] = 0
-                continue
-            if A_h[j,i] == 2:
-                A_h[i,j] = 2
-                continue
-            if within_k_nearest(i,j,NN,P_2):
-                A_h[i,j] = 1
-                continue
-            if within_k_nearest(i,j,NN,P_1) and within_k_nearest(j,i,NN,P_1):
-                A_h[i,j] = 2
-                continue
+        for j in NN[i,1:P_2+2]:
+            if j in NN[i,1:P_1+2] and i in NN[j, 1:P_1+2]:
+                    A_h[i,j] = 2
+                    A_h[j,i] = 2
             else:
-                A_h[i,j] = 0
-    return A_h
+                A_h[i,j] = 1
+    return A_h, NN
 
 def calculate_H(index, A_h):
     H = 0
+    n = len(A_h)
     for i in range(n):
         if A_h[index,i] == 2:
             H += 1
     return H
 
-def generate_bi_subcluster(index, A_h, S_bool):
+def generate_bi_subcluster(index, A_h, S):
     bi_subcluster = [index]
+    n = len(A_h)
     for j in range(n):
-        if S_bool[j] == False and A_h[index,j] == 2:
+        if j not in S and A_h[index,j] == 2:
             bi_subcluster.append(j)
     return bi_subcluster
 
@@ -72,42 +53,48 @@ def find_subclusters(D, P_1, P_2, P_3):
     label = [0] * n
     t = 0
     S = []
-    S_bool = [False] * n
     SC = []
-    A_h = calculate_affinity_matrix(D, P_1, P_2)
+    A_h, NN = calculate_affinity_matrix(D, P_1, P_2)
     for i in range(n):
         H = calculate_H(i, A_h)
-        if H <= (P1-P3):
+        if H <= (P_1-P_3):
             S.append(i)
-            S_bool[i] = True
-    for i in range(n):
-        if S_bool[i] == False:
+        else:
             if label[i] == 1:
                 continue
             else:
-                SC.append(generate_bi_subcluster(i, A_h, S_bool))
+                SC.append(generate_bi_subcluster(i, A_h, S))
                 for index in SC[t]:
                     label[index] = 1
                 t += 1
-    return SC,S
+    return SC, S, A_h, NN
 
-def calculate_mergeable(SC, NN):
+def calculate_NumBridgePoint(SC, A_h):
     k = len(SC)
-    N = np.zeros((k,k))
+    N_BP = np.zeros((k,k), dtype = int)
     for i in range(k):
         for j in range(k):
             if i != j:
+                for u in SC[i]:
+                    for v in SC[j]:
+                        if A_h[u,v] == 1:
+                            N_BP[i,j] += 1
+    return N_BP
 
 
-def mergeable(index1, index2):
-    
+def mergeable(index1, index2, N_BP):
+    if N_BP[index1,index2] == max(N_BP[index1]):
+        return True
+    else:
+        return False
 
-def merge_subclusters(SC, S, NN):
+def merge_subclusters(SC, S, NN, A_h):
     t = len(SC)
     label = [0] * t
     k = 0
     m = 0
     C = []
+    N_BP = calculate_NumBridgePoint(SC, A_h)
     for i in range(t):
         if label[i] == 0:
             k += 1
@@ -115,7 +102,7 @@ def merge_subclusters(SC, S, NN):
             C.append(SC[i])
         r = label[i]
         for j in range(t):
-            if i != j and  mergeable(i, j):
+            if i != j and  mergeable(i, j, N_BP):
                 C[r-1] += SC[j]
                 label[j] = r
     while len(S) != 0:
@@ -124,18 +111,23 @@ def merge_subclusters(SC, S, NN):
             for h in range(k):
                 if NN[l, m] in C[h]:
                     C[h].append(l)
+                    S.remove(l)
+                    break
     return C
 
 
 if __name__ == '__main__':
-
-
-
+    P_1 = [7, 5, 6, 5, 11, 5]
+    P_2 = [15, 6, 8, 5, 11, 5]
+    P_3 = [2, 2, 2, 2, 2, 2]
     ncols = 6
     plt.figure(figsize=(ncols*3, 3))
-    for col, data in enumerate(load_data()):
-        plt.subplot(1, ncols, col+1)
-        plt.scatter(data[0], data[1], s=5, alpha=0.5)
+    for i, data in enumerate(load_data()):
+        SC, S, A_h, NN = find_subclusters(data, P_1[i], P_2[i], P_3[i])
+        C = merge_subclusters(SC, S, NN, A_h)
+        plt.subplot(1, ncols, i+1)
+        for SC in C:
+            plt.scatter(data.iloc[SC][0], data.iloc[SC][1], s=5, alpha=0.5)
     plt.tight_layout()
     plt.show()
     plt.close()
