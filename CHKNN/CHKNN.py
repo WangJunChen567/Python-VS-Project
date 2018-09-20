49bfe8d64db9cde605265a0424e3d0cd55047422
Junchen Wang <31566015+WangJunChen567@users.noreply.github.com>
2018-09-20 10:08:07 +0800

    update

--- CHKNN/CHKNN.py
+++ CHKNN/CHKNN.py
@@ -46,11 +46,11 @@ def calculate_H(index, A_h):
             H += 1
     return H
 
-def generate_bi_subcluster(index, A_h, S):
+def generate_bi_subcluster(index, A_h, label_S):
     bi_subcluster = [index]
     n = len(A_h)
     for j in range(n):
-        if j not in S and A_h[index,j] == 2:
+        if label_S[j] == 0 and A_h[index,j] == 2:
             bi_subcluster.append(j)
     return bi_subcluster
 
@@ -60,18 +60,20 @@ def find_subclusters(D, P_1, P_2, P_3):
     t = 0
     S = []
     SC = []
+    label_S = [0] * n
     A_h, NN = calculate_affinity_matrix(D, P_1, P_2)
     for i in range(n):
         H = calculate_H(i, A_h)
         if H <= (P_1-P_3):
             S.append(i)
+            label_S[i] = 1
         else:
             if label[i] == 1:
                 continue
             else:
-                SC.append(generate_bi_subcluster(i, A_h, S))
-                for index in SC[t]:
-                    label[index] = 1
+                SC.append(generate_bi_subcluster(i, A_h, label_S))
+                for x in SC[t]:
+                    label[x] = 1
                 t += 1
     return SC, S, A_h, NN
 
@@ -89,7 +91,7 @@ def calculate_NumBridgePoint(SC, A_h):
 
 
 def mergeable(index1, index2, N_BP):
-    if N_BP[index1,index2] == max(N_BP[index1]):
+    if N_BP[index1,index2] != 0 and N_BP[index1,index2] == max(N_BP[index1]):
         return True
     else:
         return False
@@ -123,18 +125,29 @@ def merge_subclusters(SC, S, NN, A_h):
 
 
 if __name__ == '__main__':
-    P_1 = [7, 5, 6, 5, 11, 5]
-    P_2 = [15, 6, 8, 5, 11, 5]
+    P_1 = [11, 5, 6, 5, 11, 5]
+    P_2 = [11, 6, 8, 5, 11, 5]
     P_3 = [2, 2, 2, 2, 2, 2]
-    ncols = 6
-    plt.figure(figsize=(ncols*3, 3))
+    plt.figure(figsize=(9, 5.5))
     for i, data in enumerate(load_data()):
         SC, S, A_h, NN = find_subclusters(data, P_1[i], P_2[i], P_3[i])
-        C = merge_subclusters(SC, S, NN, A_h)
-        plt.subplot(1, ncols, i+1)
-        for SC in C:
-            plt.scatter(data.iloc[SC][0], data.iloc[SC][1], s=5, alpha=0.5)
+        #C = merge_subclusters(SC, S, NN, A_h)
+        plt.subplot(2, 3, i+1)
+        #for SCs in C:
+        plt.scatter(data.iloc[S][0],data.iloc[S][1], s=5,c='k', alpha=0.5)
+        for SCs in SC:
+            plt.scatter(data.iloc[SCs][0], data.iloc[SCs][1], s=5, alpha=0.5)
+        plt.xlabel("$x_1$")
+        plt.ylabel("$x_2$")
     plt.tight_layout()
+
+    #plt.figure(figsize=(9, 5.5))
+    #for i, data in enumerate(load_data()):
+    #    plt.subplot(2, 3, i+1)
+    #    plt.scatter(data[0], data[1], s=5, alpha=0.5)
+    #    plt.xlabel("$x_1$")
+    #    plt.ylabel("$x_2$")
+    #plt.tight_layout()
     plt.show()
     plt.close()
 
